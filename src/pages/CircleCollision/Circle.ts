d5798e7b1c9246df66ef65c068e178dfee2cf0a3
gonigh <1364389234@qq.com>
2022-11-25 19:59:17 +0800

    美化小球

--- src/pages/CircleCollision/Circle.ts
+++ src/pages/CircleCollision/Circle.ts
@@ -1,43 +1,82 @@
 import * as d3 from "d3";
+import { DefaultArcObject } from "d3";
 export class Circle {
   x: number;
   y: number;
   r: number;
   dx: number;
   dy: number;
-  el: d3.Selection<SVGCircleElement, unknown, HTMLElement, any>;
+  el: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
   right: number;
   bottom: number;
+  start: Date;
+  arcPath: d3.Arc<any, d3.DefaultArcObject>;
+  maxTime: number;
+
+  static list: Circle[];
+
   constructor(
     x: number,
     y: number,
-    r: number,
-    el: d3.Selection<SVGCircleElement, unknown, HTMLElement, any>,
+    color: string,
+    el: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
     right: number,
     bottom: number
   ) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
+    while (Circle.list.length >= 100) Circle.list.shift();
+
+    this.start = new Date();
+    this.maxTime = 10000;
     let angle = Math.random() * Math.PI * 2;
     this.dx = Math.cos(angle);
     this.dy = Math.sin(angle);
-
-    this.el = el;
     this.right = right;
     this.bottom = bottom;
+    this.r = 16;
+    if (x < this.r) x = this.r;
+    else if (x > this.right - this.r) x = this.right - this.r;
+    if (y < this.r) y = this.r;
+    else if (y > this.bottom - this.r) y = this.bottom - this.r;
+    this.x = x;
+    this.y = y;
+
+    el.append("circle")
+      .attr("r", 16)
+      .attr("stroke-width", 2)
+      .attr("stroke", color)
+      .attr("fill", "none");
+    this.arcPath = d3.arc().innerRadius(0).outerRadius(10);
+    el.append("path")
+      .attr("d", this.arcPath(<DefaultArcObject>{ startAngle: 0, endAngle: 0 }))
+      .attr("fill", color);
+    this.el = el;
+
+    Circle.list.push(this);
   }
 
   update() {
+    let time = new Date().valueOf() - this.start.valueOf();
+    if (time >= this.maxTime) {
+      this.el.remove();
+      Circle.list = Circle.list.filter((item) => item != this);
+      return;
+    }
     if (this.x > this.right - this.r || this.x < this.r) {
       this.dx = -this.dx;
     }
     if (this.y > this.bottom - this.r || this.y < this.r) {
       this.dy = -this.dy;
     }
-    console.log(this.x, this.y, this.dx, this.dy);
     this.x += this.dx;
     this.y += this.dy;
-    this.el.attr("cx", this.x).attr("cy", this.y);
+    this.el.attr("transform", `translate(${this.x},${this.y})`);
+
+    let endAngle = (time / this.maxTime) * (Math.PI * 2);
+    this.el
+      .select("path")
+      .attr(
+        "d",
+        this.arcPath(<DefaultArcObject>{ startAngle: 0, endAngle: endAngle })
+      );
   }
 }
--- src/pages/CircleCollision/index.vue
+++ src/pages/CircleCollision/index.vue
@@ -3,14 +3,16 @@ import * as d3 from 'd3'
 import { onMounted } from 'vue';
 import { Circle } from './Circle';
 
-const circleList: Circle[] = [];
 const render = () => {
-    circleList.forEach(item => item.update());
+    Circle.list.forEach(item => item.update());
     requestAnimationFrame(render);
 }
 
+Circle.list = new Array<Circle>();
+
+const colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(new Array(10).fill(0).map((item,i)=>i.toString()));
+
 onMounted(() => {
-    console.log(window);
     const container = document.getElementById('circle-collision-container');
     const width: number = <number>container?.offsetWidth;
     const height: number = <number>container?.offsetHeight;
@@ -22,19 +24,12 @@ onMounted(() => {
     svg.on('click', (e) => {
         let x = e.offsetX;
         let y = e.offsetY;
-        let r = 10
 
-        let circleElement = g.append('circle')
-            .attr('cx', x)
-            .attr('cy', y)
-            .attr('r', r)
-            .attr('fill', 'block');
-        let circle = new Circle(x, y, r, circleElement, width, height);
-        if (circleList.length > 100) {
-            circleList[0].el.remove();
-            circleList.shift();
-        }
-        circleList.push(circle);
+        let circleElement = g.append('g')
+            .attr('transform', `translate(${x},${y})`)
+
+        let circle = new Circle(x, y, colorScale((Math.random()*10).toFixed(0)), circleElement, width, height);
+
     })
     render();
 
